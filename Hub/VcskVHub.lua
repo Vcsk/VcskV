2015a13bb1acf7f032d7da9a3fd3e92278af0801
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-17 07:36:05 +0800

    Create VcskVHub.lua

---
+++ Hub/VcskVHub.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 8146731988 then
+
+else
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Notification",
+	Text = "Game Not Supported!",
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 10;
+end
