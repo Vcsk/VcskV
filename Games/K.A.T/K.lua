68b37e4199463731f193a9b21990e2f8cedbe3d3
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-17 21:55:24 +0800

    Update K.lua

--- Games/K.A.T/K.lua
+++ Games/K.A.T/K.lua
@@ -20,3 +20,12 @@ Text = "ESP Box Executed!",
 Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
 Duration = 2;
 end)
+
+s1:Button("Hitbox", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Scripts/Hitbox.lua", true))()
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "Notification",
+Text = "Hitbox Executed!",
+Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 2;
+end)
