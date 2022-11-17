04b0bbf7ce4be3fc35e6323b45ae96cd661047c9
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-17 08:50:16 +0800

    Update VcskV.lua

--- Hub/VcskV.lua
+++ Hub/VcskV.lua
@@ -10,3 +10,18 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
 Duration = 10;
 end
+
+if game.PlaceId == 0 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/Arsenal/OPArsenal", true))()
+
+else
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Notification",
+	Text = "Game Not Supported!",
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 10;
+end
+
+
