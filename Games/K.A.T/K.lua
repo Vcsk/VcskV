152985df38300f7fadc5144eecc35d7a98c8a14a
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-17 18:48:52 +0800

    Create K.lua

---
+++ Games/K.A.T/K.lua
@@ -0,0 +1,22 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+local Window = Library:NewWindow("K.A.T")
+
+local s1 = Window:NewSection("Main")
+
+s1:CreateButton("Aimbot", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/K.A.T/Aimbot.lua", true))()
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "Notification",
+Text = "Aimbot Executed!",
+Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 2;
+end)
+
+s1:CreateButton("ESP Box", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Scripts/Universal-Box-ESP.lua", true))()
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "Notification",
+Text = "ESP Box Executed!",
+Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 2;
+end)
