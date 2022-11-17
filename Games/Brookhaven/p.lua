9599e9a95b782b58de2fa33988a87a908cbd3720
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-17 10:21:34 +0800

    Create p.lua

---
+++ Games/Brookhaven/p.lua
@@ -0,0 +1,87 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "!!Important Notification!!",
+Text = "You'll need a lot of tools", 
+ 
+ 
+Button1 = "Ok",
+Duration = 30 
+})
+
+local lp = game:FindService("Players").LocalPlayer
+
+
+local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+local Window = Library:CreateWindow("Brookhaven")
+
+local s1 = Window:CreateFolder("Main")
+
+s1:Button("Bring All", function()
+
+	for i,Playys in pairs(game.Players:GetPlayers()) do
+		Playys.Character.HumanoidRootPart.CFrame = lp.Character.HumanoidRootPart.CFrame
+		lp.Character.Humanoid:Clone().Parent = lp.Character
+		lp.Character.Humanoid:Destroy()
+		game.Workspace.CurrentCamera.CameraSubject = lp.Character.Head
+	end
+	wait(0.1)
+	for i,tools in pairs(game.Players.LocalPlayer.Backpack:GetDescendants()) do
+		if tools:IsA("Tool") then
+			tools.Parent = game.Players.LocalPlayer.Character
+		end
+	end
+end) 
+
+
+s1:Button("Fling All", function()
+	for i,Playys in pairs(game.Players:GetPlayers()) do
+		Playys.Character.HumanoidRootPart.CFrame = lp.Character.HumanoidRootPart.CFrame
+		lp.Character.Humanoid:Clone().Parent = lp.Character
+		lp.Character.Humanoid:Destroy()
+		game.Workspace.CurrentCamera.CameraSubject = lp.Character.Head
+	end
+	wait(1)
+	for i,tools in pairs(game.Players.LocalPlayer.Backpack:GetDescendants()) do
+		if tools:IsA("Tool") then
+			tools.Parent = game.Players.LocalPlayer.Character
+		end
+	end
+		wait(2)
+		local Thrust = Instance.new('BodyThrust', lp.Character.HumanoidRootPart)
+		Thrust.Force = Vector3.new(9999,9999,9999)
+		Thrust.Name = "flingForce"
+end)
+
+s1:Button("Kill All", function()
+	for i,Playys in pairs(game.Players:GetPlayers()) do
+		Playys.Character.HumanoidRootPart.CFrame = lp.Character.HumanoidRootPart.CFrame
+		lp.Character.Humanoid:Clone().Parent = lp.Character
+		lp.Character.Humanoid:Destroy()
+		game.Workspace.CurrentCamera.CameraSubject = lp.Character.Head
+	end
+	wait(0.1)
+	for i,tools in pairs(game.Players.LocalPlayer.Backpack:GetDescendants()) do
+		if tools:IsA("Tool") then
+			tools.Parent = game.Players.LocalPlayer.Character
+		end
+	end
+		wait(2)
+		local Thrust = Instance.new('BodyThrust', lp.Character.HumanoidRootPart)
+		Thrust.Force = Vector3.new(99999999,99999999,9999)
+		Thrust.Name = "flingForce"
+end)
+
+s1:Button("Corruption Chat", function()
+while true do
+wait(0.6)
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I WILL ###### YOU ALL!", "ALL")
+wait(1)
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("HAHAHAHAHA! ", "ALL")
+wait(0.9)
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("HAHAHAHHAHAHAHAHAHAAHAHAHHAHAHAHAHAHAHHAHAHAHAHAHAHAHHAHAHAHAH", "ALL")
+wait(0.25)
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("HAHAHAHHAHAHAHAHAHAAHAHAHHAHAHAHAHAHAHHAHAHAHAHAHAHAHHAHAHAHAH", "ALL")
+wait(0.10)
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("YOU CAN'T STOP ME! THERES NOTHING YOU CAN DO!", "ALL")
+wait(0.6)
+   end
+end)
