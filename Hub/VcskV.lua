51fe0b1d41f4b6137d8526fdc6f163f228551d72
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-17 09:02:28 +0800

    Update VcskV.lua

--- Hub/VcskV.lua
+++ Hub/VcskV.lua
@@ -2,16 +2,7 @@ if game.PlaceId == 8146731988 then
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/UltraPowerTycoon/Script", true))()
 
-else
-
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Notification",
-	Text = "Game Not Supported!",
-	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-Duration = 10;
-end
-
-if game.PlaceId == 286090429 then
+elseif game.PlaceId == 286090429 then
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/Arsenal/OPArsenal", true))()
 
