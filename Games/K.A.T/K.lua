b332208617148076884482fd503ec5961b1f8dbb
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-18 23:33:17 +0800

    Update K.lua

--- Games/K.A.T/K.lua
+++ Games/K.A.T/K.lua
@@ -7,27 +7,27 @@ local s1 = Window:NewSection("Main")
 
 s1:CreateButton("Aimbot", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/K.A.T/Aimbot.lua", true))()
-game:GetService("StarterGui"):SetCore("SendNotification",{
-Title = "Notification",
-Text = "Aimbot Executed!",
-Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-Duration = 2;
+Notification:Notify(
+    {Title = "Notification", Description = "Executed Aimbot!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 2, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
 end)
 
 s1:CreateButton("ESP Box", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Scripts/Universal-Box-ESP.lua", true))()
-game:GetService("StarterGui"):SetCore("SendNotification",{
-Title = "Notification",
-Text = "ESP Box Executed!",
-Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-Duration = 2;
+Notification:Notify(
+    {Title = "Notification", Description = "Executed ESP Box!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 2, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
 end)
 
 s1:CreateButton("Hitbox", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Scripts/Hitbox.lua", true))()
-game:GetService("StarterGui"):SetCore("SendNotification",{
-Title = "Notification",
-Text = "Hitbox Executed!",
-Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-Duration = 2;
+Notification:Notify(
+    {Title = "Notification", Description = "Executed Hitbox!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 2, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
 end)
