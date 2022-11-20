513fcc8d1b44d2c484cfc3113b5b2b2383724cf9
Vcsk <111982858+Vcsk@users.noreply.github.com>
2022-11-20 16:24:55 +0800

    Update RedOrbs.lua

--- SC/RedOrbs.lua
+++ SC/RedOrbs.lua
@@ -50,6 +50,3 @@ game.ReplicatedStorage.rEvents.orbEvent:FireServer("collectOrb","Red Orb","City"
 game.ReplicatedStorage.rEvents.orbEvent:FireServer("collectOrb","Red Orb","City")
 game.ReplicatedStorage.rEvents.orbEvent:FireServer("collectOrb","Red Orb","City")
 game.ReplicatedStorage.rEvents.orbEvent:FireServer("collectOrb","Red Orb","City")
-game.ReplicatedStorage.rEvents.orbEvent:FireServer("collectOrb","Red Orb","City")
-game.ReplicatedStorage.rEvents.orbEvent:FireServer("collectOrb","Red Orb","City")
-game.ReplicatedStorage.rEvents.orbEvent:FireServer("collectOrb","Red Orb","City")
